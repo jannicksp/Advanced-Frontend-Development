38cfa8854dee70cc08bdf44351e425c1b2a4c3df
Jannick S. Pedersen <44465341+jannicksp@users.noreply.github.com>
2019-12-14 11:34:54 +0100

    weather api working

--- js/main.js
+++ js/main.js
@@ -38,3 +38,30 @@ function appendMenus(menus) {
   }
   document.querySelector("#menus").innerHTML += htmlTemplate;
 }
+
+// weather api Jannick
+document.addEventListener("DOMContentLoaded", function() {
+  // the DOM is fully loaded
+  console.log("Document ready!");
+
+  // url: http://api.apixu.com/v1/current.json?key=19474b792e92493e809105720180110&q=Aarhus
+
+  const url = 'http://api.weatherstack.com/current';
+  const key = 'f104663606762aadb9858a8367d0d156';
+  let query = 'Aarhus';
+
+  fetch(`${url}?access_key=${key}&query=${query}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      let current = json.current;
+      document.querySelector('#current-condition').innerHTML = `
+        <h2>${current.weather_descriptions}</h2>
+        <img src='${current.weather_icons}'>
+        <p>${current.temperature} &#8451</p>
+        <p>Feels like: ${current.feelslike} &#8451</p>
+      `;
+    });
+});
